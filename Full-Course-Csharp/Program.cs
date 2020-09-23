a66cbc387efc03d57ac9f56f53fdfbea52bf9570
Truong Khai <quockhai1610@gmail.com>
2020-09-23 14:48:19 +0700

    drawing a shape

--- Full-Course-Csharp/Program.cs
+++ Full-Course-Csharp/Program.cs
@@ -10,6 +10,15 @@ namespace Full_Course_Csharp
     {
         static void Main(string[] args)
         {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + "years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
         }
     }
 }
