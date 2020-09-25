e9a4138a50907e92d2e07b057d85ed5ef70b03be
Truong Khai <quockhai1610@gmail.com>
2020-09-25 15:03:35 +0700

    build new a guess game

--- Full-Course-Csharp/Program.cs
+++ Full-Course-Csharp/Program.cs
@@ -6,7 +6,35 @@ namespace Full_Course_Csharp
     {
         static void Main(string[] args)
         {
-            
+            string secretWord = "TruongKhai";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuess = true;
+
+            while (guess != secretWord && !outOfGuess)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuess = true;
+                }
+            }
+            if (outOfGuess)
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("You win!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
